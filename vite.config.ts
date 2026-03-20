78481559dde4431eca611570020648c1f34def60
dengsinan <1754510323@qq.com>
2026-03-20 20:07:36 +0800

    chore: 代码分割

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 import AutoImport from 'unplugin-auto-import/vite'
 import { FileSystemIconLoader } from 'unplugin-icons/loaders'
 import IconsResolver from 'unplugin-icons/resolver'
 import Icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite'
+import { name } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,8 +21,6 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
     AutoImport({
       include: [
         /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
@@ -54,10 +54,44 @@ export default defineConfig({
       },
 
     }),
+    visualizer({
+      filename: `${name}-bundle-stats.html`,
+      title: `${name} bundle stats`,
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: 'vendor-react',
+              test: /node_modules[\\/]((react($|[-\\/]))|scheduler)/,
+              priority: 30,
+            },
+            {
+              name: 'vendor-antd',
+              test: /node_modules[\\/](antd|@ant-design)/,
+              priority: 20,
+            },
+            {
+              name: 'vendor-tanstack',
+              test: /node_modules[\\/]@tanstack/,
+              priority: 15,
+            },
+            {
+              name: 'vendor',
+              test: /node_modules[\\/]/,
+              priority: 10,
+            },
+          ],
+        },
+      },
     },
   },
 })
